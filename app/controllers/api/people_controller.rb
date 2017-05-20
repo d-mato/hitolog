25706e60acf8dbe22843c3edd517e260d110d09f
Daiki Matoba <telnetstat@gmail.com>
2017-05-20 13:18:06 +0900

    Enable to update person data

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,6 +1,6 @@
 class Api::PeopleController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_person, only: [:show, :destroy]
+  before_action :set_person, only: [:show, :update, :destroy]
 
   def index
     @people = current_user.people
@@ -12,9 +12,17 @@ class Api::PeopleController < ApplicationController
   end
 
   def create
-    person = current_user.people.build(params.require(:person).permit(:name))
+    person = current_user.people.build(person_params)
     if person.save
       render json: person
+    else
+      head :bad_request
+    end
+  end
+
+  def update
+    if @person.update(person_params)
+      render json: @person
     else
       head :bad_request
     end
@@ -32,5 +40,9 @@ class Api::PeopleController < ApplicationController
 
   def set_person
     @person = current_user.people.find(params[:id])
+  end
+
+  def person_params
+    params.require(:person).permit(:name, :sex)
   end
 end
--- app/javascript/packs/components/people/show.vue
+++ app/javascript/packs/components/people/show.vue
@@ -1,13 +1,37 @@
 <template>
   <div>
     <h2>Person Detail</h2>
-    <ul>
-      <li>Name: {{ person.name }}</li>
-      <li>Sex: {{ person.sex }}</li>
-      <li>Updated at: {{ person.updated_at }}</li>
-    </ul>
+    <template v-if="!edit_mode">
+      <ul>
+        <li>Name: {{ person.name }}</li>
+        <li>Sex: {{ person.sex }}</li>
+        <li>Updated at: {{ person.updated_at }}</li>
+      </ul>
 
-    <button @click.prevent="del">del</button>
+      <button @click="edit_mode = true">edit</button>
+      <button @click="del">del</button>
+    </template>
+
+    <template v-if="edit_mode">
+      edit
+      <form @submit="save">
+        <div>
+          Name: <input v-model="person.name">
+        </div>
+        <div>
+          Sex:
+          <select v-model="person.sex">
+            <option value=""></option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <input type="submit" value="save">
+      </form>
+
+      <button @click="edit_mode = false">Cancel</button>
+    </template>
+
     <p>{{ message }}</p>
   </div>
 </template>
@@ -17,6 +41,7 @@ import axios from '../../axios'
 export default {
   data: function () {
     return {
+      edit_mode: false,
       person: {},
       message: ''
     }
@@ -31,6 +56,18 @@ export default {
       })
   },
   methods: {
+    save() {
+      axios.patch(`/api/people/${this.$route.params.id}`, this.person)
+        .then((res) => {
+          this.message = 'Success'
+          this.edit_mode = false
+        })
+        .catch((err) => {
+          this.message = 'Error'
+          console.log(err)
+        })
+    },
+
     del() {
       if (confirm('Are you sure?')) {
         axios.delete(`/api/people/${this.$route.params.id}`)
