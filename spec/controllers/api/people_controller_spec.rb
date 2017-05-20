8c67b73423c3c11caaf9865aab54db12be671346
Daiki Matoba <telnetstat@gmail.com>
2017-05-20 18:20:44 +0900

    Add tests for people api

--- spec/controllers/api/people_controller_spec.rb
+++ spec/controllers/api/people_controller_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 describe Api::PeopleController do
   let!(:user) { FactoryGirl.create :user }
-  let!(:people) { FactoryGirl.create_list :person, 10, user: user }
-  before do
-    other = FactoryGirl.create :user
-    FactoryGirl.create_list :person, 10, user: other
-  end
 
   describe 'GET index' do
+    let!(:people) { FactoryGirl.create_list :person, 10, user: user }
+    before do
+      other = FactoryGirl.create :user
+      FactoryGirl.create_list :person, 10, user: other
+    end
+
     it 'redirects if not signed in' do
       get :index, format: :json
       expect(response.status).to eq 401
@@ -18,6 +19,39 @@ describe Api::PeopleController do
       sign_in user
       get :index, format: :json
       expect(assigns(:people)).to eq people
+    end
+  end
+
+  describe 'PATCH update' do
+    let(:person) { FactoryGirl.create :person, user: user }
+    let!(:groups) { FactoryGirl.create_list(:group, 10, user: user) }
+    let(:params) do
+      {
+        id: person.id,
+        person: {
+          name: Faker::Team.name
+        },
+        group_ids: groups.sample(3).map(&:id)
+      }
+    end
+    before { sign_in user }
+
+    it 'can change person name' do
+      patch :update, format: :json, params: params
+      expect(person.reload.name).to eq params[:person][:name]
+    end
+
+    it "can change person's groups" do
+      person.update!(group_ids: groups.sample(3).map(&:id))
+      patch :update, format: :json, params: params
+      expect(person.reload.group_ids).to match_array params[:group_ids]
+    end
+
+    it '他のUserのgroupは設定できない' do
+      other_group_id = FactoryGirl.create(:group).id
+      params[:group_ids] = [other_group_id]
+      patch :update, format: :json, params: params
+      expect(person.reload.group_ids).not_to include other_group_id
     end
   end
 end
