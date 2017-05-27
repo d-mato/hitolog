40561fcbcbab3df314fad1d57933f496d3875584
Daiki Matoba <telnetstat@gmail.com>
2017-05-27 22:32:54 +0900

    Create tests for social_profile

--- spec/factories/social_profiles.rb
+++ spec/factories/social_profiles.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :social_profile do
-    user nil
-    provider "MyString"
-    uid "MyString"
-    name "MyString"
-    nickname "MyString"
-    email "MyString"
-    image_url "MyString"
-    raw_info "MyText"
+    user
+    provider 'google'
+    uid '1234567'
+    name { Faker::Name.name }
+    nickname { Faker::Name.name }
+    email { Faker::Internet.email }
+    image_url { Faker::Internet.url }
+    raw_info ''
   end
 end
--- spec/models/social_profile_spec.rb
+++ spec/models/social_profile_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
-RSpec.describe SocialProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe SocialProfile do
+  let(:social_profile) { FactoryGirl.build :social_profile }
+  describe 'validation' do
+    subject { social_profile }
+
+    context 'provider is blank' do
+      it do
+        subject.provider = nil
+        is_expected.to be_invalid
+      end
+      it do
+        subject.provider = ''
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'uid is blank' do
+      it do
+        subject.uid = nil
+        is_expected.to be_invalid
+      end
+      it do
+        subject.uid = ''
+        is_expected.to be_invalid
+      end
+    end
+
+    it 'uidとproviderが同じならinvalid' do
+      FactoryGirl.create :social_profile, provider: subject.provider, uid: subject.uid
+      is_expected.to be_invalid
+    end
+
+    it 'uidが同じでもproviderが違えばvalid' do
+      FactoryGirl.create :social_profile, provider: Faker::App.name, uid: subject.uid
+      is_expected.to be_valid
+    end
+  end
+
+  describe '#oauth_data=' do
+    let(:auth) do
+      {
+        'info' => {
+          'name' => Faker::Internet.user_name,
+          'nickname' => Faker::Name.name,
+          'email' => Faker::Internet.email,
+          'info' => Faker::Internet.url,
+        },
+        'extra' => {
+          'raw_info' => ''
+        }
+      }
+    end
+    before { social_profile.oauth_data = auth }
+    subject { social_profile }
+
+    it { is_expected.to have_attributes(name: auth['info']['name'], nickname: auth['info']['nickname'], email: auth['info']['email'], image_url: auth['info']['image_url']) }
+  end
 end
